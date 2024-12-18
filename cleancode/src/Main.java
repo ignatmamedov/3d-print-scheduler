f61ef94a3a31cca585803c7b450699492de7900b
Ignat Mamedov <548535@student.saxion.nl>
2024-12-18 13:06:44 +0100

    Added replaced cleancode folder

---
+++ cleancode/src/Main.java
@@ -0,0 +1,35 @@
+import models.Print;
+import models.Spool;
+import printers.Printer;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+import dataprovider.DataProvider;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        DataProvider dataProvider = new DataProvider();
+
+        List<Printer> printers = dataProvider.readJson("printers.json", Printer.class);
+        List<Print> prints = dataProvider.readJson("prints.json", Print.class);
+        List<Spool> spools = dataProvider.readJson("spools.json", Spool.class);
+
+        System.out.println("Printers: " + printers);
+        System.out.println("Prints: " + prints);
+        System.out.println("Spools: " + spools);
+
+
+
+//        List<Printer> printers = dataProvider.readCSV("printers.csv", true, Printer.class);
+//        List<Print> prints = dataProvider.readCSV("prints.csv", true, Print.class);
+//        List<Spool> spools = dataProvider.readCSV("spools.csv", true, Spool.class);
+//
+//        System.out.println("Printers: " + printers);
+//        System.out.println("Prints: " + prints);
+//        System.out.println("Spools: " + spools);
+
+
+
+    }
+}
---
+++ cleancode/src/types/FilamentType.java
@@ -0,0 +1,5 @@
+package types;
+
+public enum FilamentType {
+    PLA, PETG, ABS
+}
